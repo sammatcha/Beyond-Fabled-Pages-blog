969b4c814828349281541290a85502ac9ca08998
Samantha <samantha@uppointinc.com>
2024-07-14 19:29:05 -0700

    initial commit with project files

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), sitemap()],
+  output: "server",
+  adapter: vercel()
+});
