c2fe89e097ea2b714469445d036eb03bbcf49390
Samantha <samantha@uppointinc.com>
2025-01-29 15:07:24 -0800

    commenting out more for sanity

--- src/helpers/imageHelper.ts
+++ src/helpers/imageHelper.ts
@@ -1,9 +1,9 @@
-import { client } from '../sanity/client';
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityAsset } from '@sanity/image-url/lib/types/types';
+// import { client } from '../sanity/client';
+// import imageUrlBuilder from "@sanity/image-url";
+// import type { SanityAsset } from '@sanity/image-url/lib/types/types';
 
-export const builder = imageUrlBuilder(client);
+// export const builder = imageUrlBuilder(client);
 
-export function urlFor(source) {
-  return builder.image(source);
-}
+// export function urlFor(source) {
+//   return builder.image(source);
+// }
