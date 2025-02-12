df638aa3f3976ba83443083aed8c6365013556b6
Samantha <samantha@uppointinc.com>
2025-02-12 00:01:19 -0800

    fetching all posts in funsies page

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
